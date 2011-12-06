ed3fe96d7b9f4e76755c427562e0473f1e407bc9
Gergely Nagy <algernon@madhouse-project.org>
2011-12-07 00:14:37 +0100

    tests: Add more test cases.

--- tests/test-dbd-null.c
+++ tests/test-dbd-null.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include <dbi/dbi.h>
 
 int
@@ -8,6 +9,9 @@ main (int argc, char *argv[])
   dbi_conn conn;
   dbi_result result;
   int r;
+  const char *cs;
+  char *str1, *str2;
+  unsigned long long seq, n;
 
   if (argc < 2)
     {
@@ -19,8 +23,68 @@ main (int argc, char *argv[])
 
   conn = dbi_conn_new ("null");
 
+  /*
+   * Connection stuff
+   */
   r = dbi_conn_connect (conn);
   assert (r == 0);
+
+  r = dbi_conn_get_socket (conn);
+  assert (r == 0);
+
+  cs = dbi_conn_get_encoding (conn);
+  assert (strcmp (cs, "UTF-8") == 0);
+
+  /*
+   * Quoting
+   */
+  str1 = strdup ("test string");
+  str2 = str1;
+
+  r = dbi_conn_quote_string (conn, &str1);
+  assert (str1 != str2);
+  assert (strcmp (str1, "test string") == 0);
+
+  /*
+   * DB info
+   */
+  result = dbi_conn_get_db_list (conn, "%");
+  assert (result == NULL);
+
+  result = dbi_conn_get_table_list (conn, "default", "%");
+  assert (result == NULL);
+
+  r = dbi_conn_select_db (conn, "default");
+  assert (r == 0);
+
+  r = dbi_conn_ping (conn);
+  assert (r == 1);
+
+  /*
+   * Queries
+   */
+  result = dbi_conn_query (conn, "SELECT * FROM this;");
+  assert (result != NULL);
+
+  n = dbi_result_get_numrows (result);
+  assert (n == 0);
+  dbi_result_free (result);
+
+  result = dbi_conn_query_null (conn,
+                                (const unsigned char *)"SELECT * FROM this;",
+                                (unsigned long)strlen ("SELECT * FROM this;"));
+  assert (result == NULL);
+
+  seq = dbi_conn_sequence_last (conn, "defseq");
+  assert (seq == 0);
+
+  seq = dbi_conn_sequence_next (conn, "defseq");
+  assert (seq == 0);
+
+  /*
+   * Cleanup
+   */
+  dbi_conn_close (conn);
 
   dbi_shutdown ();
 
