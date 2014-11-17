7cba1eaae6817b1b2aa60033eb53bbb366c40f8f
Juhász Viktor <viktor.juhasz@balabit.com>
2014-11-17 11:03:03 +0100

    dbi-drivers: correct mysql libraries
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- sbuild/configure_opts.py
+++ sbuild/configure_opts.py
@@ -1,5 +1,5 @@
 FREETDS_OPTS = " --with-freetds --with-freetds-incdir=\${prefix}/include/ --with-freetds-libdir=\${prefix}/lib/"
-MYSQL_OPTS = " --with-mysql --enable-mysql-threadsafe --with-mysql-libdir=\${prefix}/lib/ --with-mysql-incdir=\${prefix}/include/"
+MYSQL_OPTS = " --with-mysql --enable-mysql-threadsafe --with-mysql-libdir=\${prefix}/lib/mysql --with-mysql-incdir=\${prefix}/include/mysql"
 PGSQL_OPTS = " --with-pgsql --with-pgsql-libdir=\${prefix}/lib --with-pgsql-incdir=\${prefix}/include"
 SQLITE_OPTS = " --with-sqlite3 --with-sqlite3-libdir=\${prefix}/lib --with-sqlite3-incdir=\${prefix}/include"
 ORACLE_OPTS = " --with-oracle --with-oracle-libdir=\${prefix}/oracle/lib --with-oracle-incdir=\${prefix}/oracle/include"
